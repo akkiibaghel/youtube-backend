ab3974a5775f612257e51ca4aad16d8c78db5103
Abhishek kumar <akkiibaghel2@gmail.com>
2024-06-23 23:37:49 +0530

    add new routes for user controller

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,31 +1,55 @@
 import { Router } from "express";
-import {upload} from "../middlewares/multer.middleware.js"
-import { loginUser, logoutUser, registerUser , refreshAccessToken} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  getUserChangeProfile,
+  updateUserCoverImg,
+  getWatchHistry,
+  updateUserAvatar,
+} from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import multer from "multer";
 
-const router = Router()
+const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        },
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),
-    registerUser
-)
-router.route("/login").post(loginUser)
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
+router.route("/login").post(loginUser);
 
 //secured routes
 
-router.route("/logout").post(verifyJWT ,logoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-assword").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").post(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 
-export default router
+router
+  .route("/coverImage")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImg);
 
+router.route("/c/:username").get(verifyJWT, getUserChangeProfile);
+router.route("/history").get(verifyJWT, getWatchHistry);
 
+export default router;
